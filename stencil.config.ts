b6a84b6e2bc27222da304de9e64829a07a2ba17f
Sean Wu <seanwuapps@gmail.com>
2022-02-03 17:10:54 +1100

    fix display

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,9 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { postcss } from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
+import pxtorem from 'postcss-pxtorem';
+import cssnano from 'cssnano';
 
 export const config: Config = {
   namespace: 'go-utils',
@@ -19,4 +24,21 @@ export const config: Config = {
     },
   ],
   globalScript: 'src/global/global.ts',
+  globalStyle: 'src/global/style.scss',
+  plugins: [
+    sass(),
+    postcss({
+      plugins: [
+        autoprefixer(),
+        pxtorem({
+          propList: ['*'],
+          selectorBlackList: [':root', 'html', 'body'],
+          replace: false,
+        }),
+        cssnano({
+          preset: ['default', { discardComments: { removeAll: true } }],
+        }),
+      ],
+    }),
+  ],
 };
