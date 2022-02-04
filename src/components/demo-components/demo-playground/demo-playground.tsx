8e661a5da3784424f6b600bb7e3d85cc9dda1672
Sean Wu <seanwuapps@gmail.com>
2022-02-04 18:45:20 +1100

    update demo playground

--- src/components/demo-components/demo-playground/demo-playground.tsx
+++ src/components/demo-components/demo-playground/demo-playground.tsx
@@ -7,7 +7,7 @@ export interface IProp {
   value: string | boolean | number;
   type: PropType;
   default: string | boolean | number;
-  options?: string[];
+  options?: string[] | { value: string | null; label: string }[];
 }
 @Component({
   tag: 'demo-playground',
@@ -34,12 +34,11 @@ export class DemoPlayground {
   loadedComponent: HTMLElement;
 
   log(...args: any[]) {
-    console.log('[Demo Playground]', ...args);
+    console.trace('[Demo Playground]', ...args);
   }
 
   componentWillLoad() {
     this.propsArray = typeof this.props === 'string' ? JSON5.parse(this.props) : this.props;
-    this.log(this.propsArray);
   }
 
   private targetEl = null;
@@ -67,6 +66,10 @@ export class DemoPlayground {
 
   applyProps() {
     this.propsArray.forEach(({ name, value }) => {
+      if (value === 'null' || value === null) {
+        this.targetEl.removeAttribute(name);
+        return;
+      }
       this.targetEl[name] = value;
     });
 
@@ -76,6 +79,9 @@ export class DemoPlayground {
   getUsage() {
     return `<${this.tag} ${this.propsArray
       .map(({ name, value, type }) => {
+        if (value === 'null' || value === null) {
+          return false;
+        }
         if (type === 'boolean' && !value) {
           return false;
         }
@@ -103,16 +109,18 @@ export class DemoPlayground {
 
   updatePropValue(e: Event, propObject: IProp) {
     const { name, type } = propObject;
-    this.propsArray = this.propsArray.map((p) => {
-      if (p.name === name) {
-        if (type === 'boolean') {
-          p.value = (e.target as HTMLInputElement).checked;
-        } else {
-          p.value = (e.target as HTMLInputElement).value;
+    this.propsArray = this.propsArray
+      .map(p => {
+        const newValue = type === 'boolean' ? (e.target as HTMLInputElement).checked : (e.target as HTMLInputElement).value;
+        if (p.name === name) {
+          if (newValue === null) {
+            return null;
+          }
+          p.value = newValue;
         }
-      }
-      return p;
-    });
+        return p;
+      })
+      .filter(Boolean);
   }
 
   renderPropControl(propObject: IProp) {
@@ -137,10 +145,32 @@ export class DemoPlayground {
       return (
         <div class="prop-control">
           <label htmlFor={name}>{name}</label>
-          <select class="input" id={name} onInput={(e) => this.updatePropValue(e, propObject)}>
-            {options.map((value) => (
-              <option value={value}>{value}</option>
-            ))}
+          <select class="input" id={name} onInput={e => this.updatePropValue(e, propObject)}>
+            {options.map(option => {
+              if (option === null) {
+                return <option value={null}>null</option>;
+              }
+              if (typeof option === 'string') {
+                return (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                );
+              }
+              const { value, label } = option;
+              if (!label) {
+                return (
+                  <option key={value} value={value}>
+                    {value}
+                  </option>
+                );
+              }
+              return (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              );
+            })}
           </select>
         </div>
       );
@@ -154,11 +184,20 @@ export class DemoPlayground {
           class="input"
           type={inputType}
           value={value as string}
-          onInput={(e) => this.updatePropValue(e, propObject)}
+          onInput={e => this.updatePropValue(e, propObject)}
           checked={type === 'boolean' && value !== false}
         />
       </div>
     );
+  }
+
+  @State() showConfigPanel = true;
+
+  closeConfigPanel() {
+    this.showConfigPanel = false;
+  }
+  openConfigPanel() {
+    this.showConfigPanel = true;
   }
 
   render() {
@@ -166,17 +205,38 @@ export class DemoPlayground {
     return (
       <Host>
         <div class="container">
-          <div class="row">
-            <div class="col-tablet-8 demo">
+          <div class="row demo-row">
+            <div class="col demo">
               <div class="demo-bg"></div>
               <div class="demo-content">
                 <slot></slot>
               </div>
+              {!this.showConfigPanel ? (
+                <go-button round class={{ 'control-panel-opener': true }} color="tertiary" icon flat onClick={() => this.openConfigPanel()}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                    <circle cx="12" cy="12" r="3" />
+                    <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
+                  </svg>
+                </go-button>
+              ) : null}
             </div>
-            <div class="col-tablet-4 controls">
+            <div
+              class={{
+                'col-tablet-4 control-panel': true,
+                'show': this.showConfigPanel,
+              }}
+            >
+              <div class="control-header">
+                <span>Configuration</span>
+                <go-button round compact color="tertiary" flat icon onClick={() => this.closeConfigPanel()}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                    <path d="M18 6 6 18M6 6l12 12" />
+                  </svg>
+                </go-button>
+              </div>
               <div class="props">
                 {debug ? <pre>{JSON5.stringify(this.propsArray, undefined, 2)}</pre> : null}
-                {this.propsArray.map((propObj) => {
+                {this.propsArray.map(propObj => {
                   return (
                     <div class="prop" key={propObj.name}>
                       <div>{this.renderPropControl(propObj)}</div>
@@ -189,8 +249,8 @@ export class DemoPlayground {
           <div class="row">
             <div class="col-tablet-12">
               <div class="usage">
-                <go-accordion>
-                  <go-accordion-item headingTag="span" heading="Output">
+                <go-accordion multiple>
+                  <go-accordion-item heading="Output" active={true}>
                     <pre>
                       <code>{this.getUsage()}</code>
                     </pre>
