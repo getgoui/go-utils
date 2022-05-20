9d535bfd6ba2599655fc1ae9db45b43acd8010fc
Sean Wu <seanwuapps@gmail.com>
2022-05-21 08:56:01 +1000

    update demo control btn

--- src/components/demo-components/demo-controls/demo-controls.tsx
+++ src/components/demo-components/demo-controls/demo-controls.tsx
@@ -19,14 +19,7 @@ export class DemoControls {
   render() {
     return (
       <Host>
-        <go-button
-          aria-labelledby="demo-control-tooltip"
-          id="demo-control-btn"
-          type="button"
-          onClick={() => this.open()}
-          icon
-          round
-        >
+        <go-button class="demo-control-btn" type="button" onClick={() => this.open()} compact>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -38,10 +31,8 @@ export class DemoControls {
           >
             <path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z" />
           </svg>
+          <span>Demo controls</span>
         </go-button>
-        <go-tooltip id="demo-control-tooltip" placement="left" trigger-id="demo-control-btn">
-          Open demo controls
-        </go-tooltip>
         <go-nav-drawer position="right" active={this.isOpen} label="Demo controls" onClose={() => this.close()}>
           <div class="demo-control-drawer">
             <div class="demo-control-header">
